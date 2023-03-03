4e6275fa2c9adf47aeddd8af32342a146743cfcd
Allen Liu <alex03108861@gmail.com>
2023-03-03 16:29:54 +0800

    Chat GPT Version

--- app.py
+++ app.py
@@ -11,25 +11,31 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 def index():
     if request.method == "POST":
         animal = request.form["animal"]
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[{"role": "user", "content": generate_prompt(animal)}],
+            temperature=0.8,
         )
-        return redirect(url_for("index", result=response.choices[0].text))
+        return redirect(url_for("index", result=response.choices[0].message.content))
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
 
 
 def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
+    return """請使用正體中文(台灣) 產生關於某超級英雄動物的名稱，格式為： 超級英雄：「產生的名稱！！！」
+    英雄名字例如： 
+    美国队长（Captain America） ...
+    钢铁侠（Iron Man） ...
+    雷神（Thor） ...
+    绿巨人（Hulk） ...
+    黑寡妇（Black Widow） ...
+    鹰眼（Hawkeye） ...
+    死侍（Deadpool） ...
+    毒液（Venom）
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
+    題目是：
+        動物: {}
+        名字:""".format(
+                animal.capitalize()
     )
